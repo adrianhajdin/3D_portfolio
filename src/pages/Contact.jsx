25814dcd02f4289f982f24bb01cd8e570b9d045d
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-10-21 01:06:27 +0530

    feat: fox position and other changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,34 +1,29 @@
-import { Suspense, useRef, useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
 
 import { Fox } from "../models/Fox";
 import { Loader } from "../components";
 
 const Contact = () => {
-  const form = useRef();
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState("idle");
-  const [notificationMessage, setNotificationMessage] = useState(null);
-  const [foxPosition, setFoxPosition] = useState([0, 0, 0]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 480) {
-        setFoxPosition([2, 1, -7.6]); // set the position of the fox for mobile screens
-      } else {
-        setFoxPosition([0, 0, 0]); // set the default position for larger screens
-      }
-    };
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
 
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-
-    // Bind the event listener
-    window.addEventListener("resize", handleResize);
-
-    // Unbind the event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array to ensure that effect is only run on mount and unmount
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   const handleFocus = () => {
     setCurrentAnimation("walk");
@@ -38,8 +33,9 @@ const Contact = () => {
     setCurrentAnimation("idle");
   };
 
-  const sendEmail = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
 
     emailjs
       .send(
@@ -56,39 +52,45 @@ const Contact = () => {
       )
       .then(
         () => {
-          setNotificationMessage("Message sent successfully");
-          setCurrentAnimation("hit"); // play hit animation
-          // Hide notification after 3 seconds
-          setTimeout(() => setNotificationMessage(null), 3000);
+          setCurrentAnimation("hit");
+
+          setLoading(false);
+          setTimeout(() => {
+            alert("Thank you. I will get back to you as soon as possible.");
+            setCurrentAnimation("idle");
+          }, [3000]);
         },
         (error) => {
-          setNotificationMessage("Failed to send message");
-          // Hide notification after 3 seconds
-          setTimeout(() => setNotificationMessage(null), 3000);
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
         }
       );
   };
 
   return (
-    <section className='flex flex-row max-w-5xl mx-auto sm:p-16 pb-12 !pt-[126px] px-8 min-h-[calc(100vh-100px)]'>
-      {notificationMessage && (
-        <div className='notification'>{notificationMessage}</div>
-      )}
-
-      <div className='flex-1 flex flex-col'>
+    <section className='flex lg:flex-row flex-col max-w-5xl mx-auto sm:p-16 pb-12 !pt-[126px] px-8 min-h-[calc(100vh-100px)]'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='sm:text-5xl text-3xl font-extrabold sm:leading-snug font-manrope'>
           Get in Touch
         </h1>
 
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='w-full flex flex-col gap-7 mt-14'
+        >
           <label className='text-[#1D2235] font-semibold'>
             Name
             <input
               type='text'
-              id='first_name'
-              class='bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card'
+              name='name'
+              className='bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card'
               placeholder='John'
               required
+              value={form.name}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
@@ -97,10 +99,12 @@ const Contact = () => {
             Email
             <input
               type='email'
-              id='first_name'
-              class='bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card'
+              name='email'
+              className='bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2.5 font-normal shadow-card'
               placeholder='John@gmail.com'
               required
+              value={form.email}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
@@ -108,10 +112,12 @@ const Contact = () => {
           <label className='text-[#1D2235] font-semibold'>
             Your Message
             <textarea
-              id='message'
+              name='message'
               rows='4'
-              class='block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-200 focus:ring-blue-500 focus:border-blue-500 mt-2.5 font-normal shadow-card'
+              className='block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-200 focus:ring-blue-500 focus:border-blue-500 mt-2.5 font-normal shadow-card'
               placeholder='Write your thoughts here...'
+              value={form.message}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
@@ -119,16 +125,17 @@ const Contact = () => {
 
           <button
             type='submit'
-            class='text-white bg-gradient-to-r from-[#00c6ff] to-[#0072ff] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+            disabled={loading}
+            className='text-white bg-gradient-to-r from-[#00c6ff] to-[#0072ff] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
             onFocus={handleFocus}
             onBlur={handleBlur}
           >
-            Submit
+            {loading ? "Sending..." : "Submit"}
           </button>
         </form>
       </div>
 
-      <div className='flex-1'>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
         <Canvas
           camera={{
             position: [0, 0, 5],
@@ -137,15 +144,21 @@ const Contact = () => {
             far: 1000,
           }}
         >
+          <directionalLight position={[0, 0, 1]} intensity={1.5} />
           <ambientLight intensity={0.5} />
-          <pointLight position={[10, 10, 10]} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
 
           <Suspense fallback={<Loader />}>
             <Fox
               currentAnimation={currentAnimation}
-              position={foxPosition}
-              rotation={[12.629, 5.254, 0]}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.629, -0.5, 0]}
               scale={[0.45, 0.45, 0.45]}
             />
           </Suspense>
