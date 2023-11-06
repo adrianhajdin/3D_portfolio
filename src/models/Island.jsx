75cbb0cfa85fdc40d4bb218ed2e017bd0094b55e
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-06 16:15:27 +0530

    feat: reduce speed of rotation for keys

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -83,13 +83,13 @@ export function Island({
     if (event.key === "ArrowLeft") {
       if (!isRotating) setIsRotating(true);
 
-      islandRef.current.rotation.y += 0.01 * Math.PI;
-      rotationSpeed.current = 0.0125;
+      islandRef.current.rotation.y += 0.005 * Math.PI;
+      rotationSpeed.current = 0.007;
     } else if (event.key === "ArrowRight") {
       if (!isRotating) setIsRotating(true);
 
-      islandRef.current.rotation.y -= 0.01 * Math.PI;
-      rotationSpeed.current = -0.0125;
+      islandRef.current.rotation.y -= 0.005 * Math.PI;
+      rotationSpeed.current = -0.007;
     }
   };
 
